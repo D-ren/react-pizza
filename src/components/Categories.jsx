3e554cba64d7ae7fd0529a63c5d5d5957dbc2d65
D-ren <50651044+D-ren@users.noreply.github.com>
2022-08-28 16:43:16 +0300

    initialized with started data

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export default function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const categories = [
+    'Все', 
+    'Мясные', 
+    'Вегетарианская', 
+    'Гриль', 
+    'Острые', 
+    'Закрытые',
+  ]
+
+  function onClickCategory(index) {
+    setActiveIndex(index)
+  }
+
+  return (
+    <div className="categories">
+    <ul>
+      {
+        categories.map((value, index) => (
+          <li key={value} onClick={() => onClickCategory(index)} className={activeIndex ===index ? 'active': ''}>
+            {value}
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+  )
+}
